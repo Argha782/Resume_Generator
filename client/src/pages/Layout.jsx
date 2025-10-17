58f7bc24a1d8db4a359380c6d00c8bd7a88f03c9
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-10-17 21:18:25 +0530

    Frontend setup

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+function Layout() {
+    return (
+        <div>
+            <div className='min-h-screen bg-gray-50f'>
+                <Navbar/>
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
+
+export default Layout
